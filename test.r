4b70089726f4bf398315539f895f6ac98bcd6024
Joseph DeCommer <jdecommer@gmail.com>
2024-06-26 09:33:06 -0400

    learning how to plot binomial dist. in R

--- test.r
+++ test.r
@@ -1,14 +1,7 @@
-library(data.table)
-
-set.seed(123)
-n = 10000
-dt <- data.table(id = 1:n)
-dt[, x1 := rnorm(.N, mean = 0, sd = 2)]
-dt[, x2 := runif(.N, min = -1, max = 1)]
-dt[, y := 2 * x1 + x2 + 0.5 * rnorm(.N)]
-
-model <- lm(y ~ x1 + x2, data = dt)
-rows <- 1:nrow(dt)
-summary(model)
-
-plot(model)
+success <- 0:50
+plot(success,dbinom(success,size=50,prob=.45),
+     type='h',
+     main='Binomial Distribution (n=50, p=0.45)',
+     ylab='Probability',
+     xlab ='Successes',
+     lwd=3)
