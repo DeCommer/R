55aa4422ac23745ef5806fa8246b966a6f0bc62c
Joseph DeCommer <jdecommer@gmail.com>
2024-06-27 19:14:45 -0400

    R practice

--- test.r
+++ test.r
@@ -1,7 +1,27 @@
-success <- 0:50
-plot(success,dbinom(success,size=50,prob=.45),
-     type='h',
-     main='Binomial Distribution (n=50, p=0.45)',
-     ylab='Probability',
-     xlab ='Successes',
-     lwd=3)
+library(tidyverse)
+
+#starwars dataset
+view(starwars)
+glimpse(starwars)
+
+fat_characters <- filter(starwars, mass > 100)
+view(fat_characters)
+
+view(filter(starwars, films == 'A New Hope'))
+
+ggplot(starwars, aes(x = mass)) +
+    geom_histogram() +
+    geom_freqpoly()
+
+#mpg dataset
+view(mpg)
+glimpse(mpg)
+
+view(filter(mpg, manufacturer == 'ford'))
+
+ggplot(mpg, aes(x = cty)) +
+    geom_histogram() +
+    geom_freqpoly()
+
+ggplot(mpg, aes(x = cty, y = hwy, color = class)) +
+    geom_point()
